61af9354be37339677a09e4961ee3467aa0a7b0f
Anja Papatola <mamadev@apalopta.de>
2024-12-01 21:18:26 +0100

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,32 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun convertToLists(list: List<String>): Pair<List<Int>, List<Int>> {
+        val l1 = mutableListOf<Int>()
+        val l2 = mutableListOf<Int>()
+
+        list.map { li -> li.split("   ").map { it.toInt() } }
+            .forEach { (a, b) ->
+                l1.add(a)
+                l2.add(b)
+            }
+
+        return Pair(l1.sorted(), l2.sorted())
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(a: List<Int>, b: List<Int>): Long =
+        a.zip(b).sumOf { (anum, bnum) -> abs(anum - bnum).toLong() }
+
+    fun part2(a: List<Int>, b: List<Int>): Int =
+        a.sumOf { anum -> b.count { bnum -> bnum == anum } * anum }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val (list1, list2) = splitToLists(readInput("test_input"))
+//    println(part2(list1, list2))
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val (a, b) = convertToLists(readInput("Day01"))
+    part1(a, b).println()
+    part2(a, b).println()
 }
