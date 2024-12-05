a28c4cd61145e34ce35eb9ae776ca5fbb6c2a1b7
Anja Papatola <mamadev@apalopta.de>
2024-12-05 13:47:51 +0100

    refactor Day05

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,92 +1,81 @@
 package day05
 
-import println
 import readInput
-import readTestInput
+
+data class Rule(val first: Int, val second: Int)
+typealias Rules = List<Rule>
+typealias Update = List<Int>
 
 fun main() {
-    data class Ordering(val first: Int, val second: Int)
-    data class Rules(val orderings: List<Ordering>)
-    data class Update(val pages: List<Int>)
 
-    fun Update.matchingOrderings(rules: Rules): List<Ordering> =
-        rules.orderings.filter { it.first in this.pages && it.second in this.pages }
+    fun Rules.matchingFor(update: Update): Rules = filter { it.first in update && it.second in update }
 
-    fun sequenceFromOrdering(orderings: List<Ordering>): List<Int> {
+    fun validOrderFromRules(rules: Rules): List<Int> {
         val sequence = mutableListOf<Int>()
-        val myOrderings = orderings.toMutableList()
+        val myRules = rules.toMutableList()
 
-        while (myOrderings.isNotEmpty()) {
-            if (myOrderings.size == 1) {
-                sequence.add(myOrderings[0].first)
-                sequence.add(myOrderings[0].second)
-                myOrderings.removeAt(0)
+        // the current very first page cannot appear anywhere as 'second'
+        // remove all rules that contain the current very first page as first entry
+        // then the remaining rules must contain a new very first page
+        // the last remaining rule contains the last two pages
+        while (myRules.isNotEmpty()) {
+            if (myRules.size == 1) {
+                sequence.add(myRules[0].first)
+                sequence.add(myRules[0].second)
+                myRules.removeAt(0)
             } else {
-                val veryFirst = myOrderings.first { ordering -> ordering.first !in myOrderings.map { it.second } }.first
+                // the next very first page is the one 'first' of the remaining rules that is not found in any rule's second entry
+                val veryFirst = myRules.first { rule -> rule.first !in myRules.map { it.second } }.first
                 sequence.add(veryFirst)
-                myOrderings.removeAll { ordering -> ordering.first == veryFirst }
+                // rules with this page as first entry are obsolete now
+                myRules.removeAll { rule -> rule.first == veryFirst }
             }
         }
 
         return sequence.toList()
     }
 
-    fun part1(lines: List<String>): Int {
-        val rules = Rules(lines.filter { it.contains("|") }
-            .map { Ordering(it.substringBefore("|").toInt(), it.substringAfter("|").toInt()) })
+    fun List<String>.getRules() =
+        filter { it.contains("|") }
+            .map { Rule(it.substringBefore("|").toInt(), it.substringAfter("|").toInt()) }
 
-        val updates = lines.filter { it.contains(",") }.map { line ->
-            line.split(",").map { it.toInt() }
-        }.map { Update(it) }
+    fun List<String>.getUpdates() =
+        filter { it.contains(",") }
+            .map { line -> line.split(",").map { it.toInt() } }
+
+    fun solve(lines: List<String>): Pair<Int, Int> {
+        val rules = lines.getRules()
+        val updates = lines.getUpdates()
 
         val validUpdates = mutableListOf<Update>()
-        updates.forEach { update ->
-            val matchingOrderings = update.matchingOrderings(rules).toMutableList()
-            val validSequence = sequenceFromOrdering(matchingOrderings)
-            if (update.pages == validSequence) {
-                validUpdates.add(update)
-            }
-        }
-
-        return validUpdates.sumOf {
-            val pages = it.pages
-            pages[(pages.size / 2)]
-        }
-    }
-
-    fun part2(lines: List<String>): Int {
-        val rules = Rules(lines.filter { it.contains("|") }
-            .map { Ordering(it.substringBefore("|").toInt(), it.substringAfter("|").toInt()) })
-
-        val updates = lines.filter { it.contains(",") }.map { line ->
-            line.split(",").map { it.toInt() }
-        }.map { Update(it) }
-
         val invalidCorrectedUpdates = mutableListOf<Update>()
+
         updates.forEach { update ->
-            val matchingOrderings = update.matchingOrderings(rules).toMutableList()
-            val validSequence = sequenceFromOrdering(matchingOrderings)
-            if (update.pages != validSequence) {
-                invalidCorrectedUpdates.add(Update(validSequence))
+            val validOrder = validOrderFromRules(rules.matchingFor(update))
+            if (update == validOrder) {
+                validUpdates.add(update)
+            } else {
+                invalidCorrectedUpdates.add(validOrder)
             }
         }
 
-        return invalidCorrectedUpdates.sumOf {
-            val pages = it.pages
-            pages[(pages.size / 2)]
-        }
+        return Pair(
+            validUpdates.sumOf { it[(it.size / 2)] },
+            invalidCorrectedUpdates.sumOf { it[(it.size / 2)] }
+        )
     }
 
     // Test if implementation meets criteria from the description, like:
-    val testInput = readTestInput("day05/test_input")
-    println("=== TEST ====")
-    println(part1(testInput))
-    println(part2(testInput))
+//    val testInput = readTestInput("day05/test_input")
+//    val testResult = solve(testInput)
+//    println("=== TEST ====")
+//    println(testResult.first)
+//    println(testResult.second)
 
     val input = readInput("Day05")
 
+    val (valid, invalidCorrected) = solve(input)
     println("=== RES ====")
-    println(part1(input))
-    println(part2(input))
-
+    println(valid)
+    println(invalidCorrected)
 }
