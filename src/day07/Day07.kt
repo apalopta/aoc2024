cb03f4ad175223f54a27e07d6076887b69e34a20
Anja Papatola <mamadev@apalopta.de>
2024-12-07 17:12:46 +0100

    Day07 with coroutines

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -1,56 +1,48 @@
 package day07
 
+import kotlinx.coroutines.*
 import utils.*
 import kotlin.time.measureTimedValue
 
-infix fun Long.`||`(other: Long): Long = "$this$other".toLong()
-
-data class Equation(val res: Long, val numbers: List<Long>)
-
 fun main() {
+
+    data class Equation(val res: Long, val numbers: List<Long>)
+
+    infix fun Long.`||`(other: Long): Long = "$this$other".toLong()
 
     fun String.toEquation() = Equation(
         substringBefore(':').toLong(),
         substringAfter(": ").split(' ').map { it.trim().toLong() }
     )
 
-    fun solve(lines: List<String>, block: (Long, Long) -> List<Long>): Long {
-        val equations = lines.map { it.toEquation() }
-        val correctEquations = mutableListOf<Long>()
-
-        equations.forEach { equation ->
-            var possibleResults = listOf(equation.numbers.first())
-            for ((i, num) in equation.numbers.withIndex()) {
-                if (i > 0) {
-                    possibleResults = possibleResults.flatMap { block(it, num) }
+    fun solve(lines: List<String>, opVariants: (Long, Long) -> List<Long>) = runBlocking {
+        lines.map { line ->
+            async(Dispatchers.Default) {
+                val equation = line.toEquation()
+                var possibleResults = listOf(equation.numbers.first())
+                for ((i, num) in equation.numbers.withIndex()) {
+                    if (i > 0) {
+                        possibleResults = possibleResults.flatMap { opVariants(it, num) }
+                    }
+                }
+                if (possibleResults.contains(equation.res)) {
+                    equation.res
+                } else {
+                    null
                 }
             }
-            if (possibleResults.contains(equation.res)) {
-                correctEquations.add(equation.res)
-            }
-        }
-
-        return correctEquations.sum()
+        }.awaitAll().mapNotNull { it }.sum()
     }
-
-
-    // Test if implementation meets criteria from the description, like:
-//    val testInput = readTestInput("day07/test_input")
-//    println("=== TEST ===")
-//    println(part1(testInput))
-//    println(part2(testInput)) // 11387
 
     val input = readInput("Day07")
-    println("=== RESULT ===")
 
-    val timed1 = measureTimedValue {
-        solve(input) { a, b -> listOf(a + b, a * b) }
+    listOf(
+        { a: Long, b: Long -> listOf(a + b, a * b) },           // 1620690235709 (w/o coroutines: 127 ms, w/ coroutines: ~200 ms)
+        { a: Long, b: Long -> listOf(a + b, a * b, a `||` b) }  // 145397611075341 (w/o coroutines: ~1.3 sec, w/ coroutines: ~750 ms)
+    ).forEach { opVariants ->
+        val (result, duration) = measureTimedValue {
+            solve(input, opVariants)
+        }
+        println("$result (${duration.inWholeMilliseconds} ms)")
     }
-    println("${timed1.value} (${timed1.duration.inWholeMilliseconds} ms)") // 1620690235709 (127 ms)
-
-    val timed2 = measureTimedValue {
-        solve(input) { a, b -> listOf(a + b, a * b, a `||` b) }
-    }
-    println("${timed2.value} (${timed2.duration.inWholeMilliseconds} ms)") // 145397611075341 (~1.3 sec)
-
 }
